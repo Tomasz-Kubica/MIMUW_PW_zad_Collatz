3467b563e693d777f9ea731f682c3cb1ff39b041
Tomasz Kubica <tk429368@students.mimuw.edu.pl>
2022-01-30 22:13:06 +0100

    jakaś część implementacji

--- sharedresults.hpp
+++ sharedresults.hpp
@@ -1,9 +1,45 @@
 #ifndef SHAREDRESULTS_HPP
 #define SHAREDRESULTS_HPP
 
+#include <utility>
+#include <deque>
+#include <future>
+#include <map>
+
+#include "collatz.hpp"
+#include "lib/infint/InfInt.h"
+
 class SharedResults
 {
-//TODO
+public:
+    using getResult = std::pair<bool, uint64_t>;
+
+     getResult get(InfInt key) {
+         getResult r;
+         mut.lock();
+         auto it = results.find(key);
+         if (it != results.end()) {
+             r = {false, 0};
+         } else {
+             r = {true, it->second};
+         }
+         mut.unlock();
+         return r;
+    }
+
+    void set(InfInt key, uint64_t value) {
+         mut.lock();
+         auto it = results.find(key);
+         if (it == results.end()) {
+             results.insert({key, value});
+         }
+         mut.unlock();
+     }
+private:
+    using mapValue = std::pair<uint64_t, std::mutex>;
+
+    std::mutex mut;
+    std::map<InfInt, uint64_t> results;
 };
 
 #endif
--- teams.cpp
+++ teams.cpp
@@ -5,44 +5,87 @@
 #include "teams.hpp"
 #include "contest.hpp"
 #include "collatz.hpp"
+#include "sharedresults.hpp"
 
-ContestResult TeamNewThreads::runContestImpl(ContestInput const & contestInput)
-{
+uint64_t collatzWithShared(InfInt input, SharedResults *results) {
+
+}
+
+inline void
+teamNewThreadsThread(InfInt input, uint64_t *result, std::mutex *mut,
+                     std::condition_variable *condActive,
+                     uint64_t *activeThreads) {
+    *result = calcCollatz(input);
+    mut->lock();
+    activeThreads--;
+    condActive->notify_one();
+    mut->unlock();
+}
+
+ContestResult TeamNewThreads::runContestImpl(ContestInput const &contestInput) {
+    ContestResult r;
+    r.resize(contestInput.size());
+    uint64_t activeThreads = 0;
+    std::mutex mut;
+    std::condition_variable condActive;
+
+    std::vector<std::thread> threads;
+    size_t i = 0;
+    for (const InfInt &singleInput: contestInput) {
+        {
+            std::unique_lock<std::mutex> lock(mut);
+            if (activeThreads >= getSize()) {
+                condActive.wait(lock, [&activeThreads, this] {
+                    return activeThreads >= this->getSize();
+                });
+            }
+            activeThreads++;
+        }
+        auto t = createThread(teamNewThreadsThread, singleInput, &r[i], &mut,
+                              &condActive, &activeThreads);
+        t.detach();
+        i++;
+    }
+
+    // oczekiwanie na zakończenie pracy przez wszystkie wątki
+    {
+        std::unique_lock<std::mutex> lock(mut);
+        if (activeThreads > 0) {
+            condActive.wait(lock, [&activeThreads] {
+                return activeThreads > 0;
+            });
+        }
+    }
+
+    return r;
+}
+
+ContestResult
+TeamConstThreads::runContestImpl(ContestInput const &contestInput) {
     ContestResult r;
     //TODO
     return r;
 }
 
-ContestResult TeamConstThreads::runContestImpl(ContestInput const & contestInput)
-{
+ContestResult TeamPool::runContest(ContestInput const &contestInput) {
     ContestResult r;
     //TODO
     return r;
 }
 
-ContestResult TeamPool::runContest(ContestInput const & contestInput)
-{
+ContestResult TeamNewProcesses::runContest(ContestInput const &contestInput) {
     ContestResult r;
     //TODO
     return r;
 }
 
-ContestResult TeamNewProcesses::runContest(ContestInput const & contestInput)
-{
+ContestResult TeamConstProcesses::runContest(ContestInput const &contestInput) {
     ContestResult r;
     //TODO
     return r;
 }
 
-ContestResult TeamConstProcesses::runContest(ContestInput const & contestInput)
-{
-    ContestResult r;
-    //TODO
-    return r;
-}
-
-ContestResult TeamAsync::runContest(ContestInput const & contestInput)
-{
+ContestResult TeamAsync::runContest(ContestInput const &contestInput) {
     ContestResult r;
     //TODO
     return r;
