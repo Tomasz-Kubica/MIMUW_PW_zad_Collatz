0a642331f2417c48403ba8260a258ac69e33b1d1
Tomasz Kubica <tk429368@students.mimuw.edu.pl>
2022-02-11 14:09:38 +0100

    Added shared_mem_info.h

---
+++ shared_mem_info.h
@@ -0,0 +1,12 @@
+#ifndef COLLATZ_SHARED_MEM_INFO_H
+#define COLLATZ_SHARED_MEM_INFO_H
+
+#define PIPE_READ 0
+#define PIPE_WRITE 1
+
+#define SHM_NAME "/collatz_process_memory"
+#define SEM_NAME "/collatz_process_semaphore"
+
+#define SHM_SIZE 1
+
+#endif //COLLATZ_SHARED_MEM_INFO_H
