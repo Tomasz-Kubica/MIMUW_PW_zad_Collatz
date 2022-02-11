d60c7c5b407c048a35ed27ca07d3c6367cde9ec2
Tomasz Kubica <tk429368@students.mimuw.edu.pl>
2022-02-11 20:22:19 +0100

    Usunięto niepotrzebne zakomentowane fragmenty kodu

--- teams.cpp
+++ teams.cpp
@@ -18,21 +18,16 @@
 
 uint64_t
 collatzWithShared(InfInt input, std::shared_ptr<SharedResults> results) {
-    //std::cout << "start " << std::this_thread::get_id() << std::endl;
     assert(input > 0);
     uint64_t count = 0;
     std::vector<std::pair<InfInt, uint64_t>> reserved;
     while (input > 1) {
-        //std::cout << input << std::endl;
         SharedResults::getResult getRes = results->getOrReserve(input);
-        //std::cout << "get" << std::endl;
         if (getRes.first) {
             count += getRes.second;
-            //std::cout << "break" << std::endl;
             break;
         } else {
             reserved.push_back({input, count});
-            //std::cout << "reserved" << std::endl;
         }
         if (input % 2 == 1) {
             input *= 3;
@@ -45,7 +40,6 @@ collatzWithShared(InfInt input, std::shared_ptr<SharedResults> results) {
     for (auto res: reserved) {
         results->setReserved(res.first, count - res.second);
     }
-    //std::cout << "koniec " << std::this_thread::get_id() << std::endl;
     return count;
 }
 
@@ -69,11 +63,7 @@ teamConstThreadsThread(ContestInput::const_iterator input,
                        ContestResult::iterator result,
                        size_t to_proces,
                        std::shared_ptr<SharedResults> shared
-//                       std::mutex *mut,
-                       //std::condition_variable *cv,
-                       //int *active
                        ) {
-    //std::cout << "start: " << std::this_thread::get_id() << std::endl;
     for (int i = 0; i < to_proces; i++) {
         if (shared)
             *result = collatzWithShared(*input, shared);
@@ -82,12 +72,6 @@ teamConstThreadsThread(ContestInput::const_iterator input,
         input++;
         result++;
     }
-//    mut->lock();
-//    (*active)--;
-//    if (*active == 0)
-//        cv->notify_all();
-//    mut->unlock();
-    //std::cout << "koniec: " << std::this_thread::get_id() << std::endl;
 }
 
 ContestResult TeamNewThreads::runContestImpl(ContestInput const &contestInput) {
@@ -101,15 +85,10 @@ ContestResult TeamNewThreads::runContestImpl(ContestInput const &contestInput) {
     for (const InfInt &singleInput: contestInput) {
         {
             std::unique_lock<std::mutex> lock(mut);
-            //std::cout << "czeka na start wątku " << activeThreads << std::endl;
             if (activeThreads >= getSize()) {
                 condActive.wait(lock, [&activeThreads, this] {
                     return activeThreads < this->getSize();
                 });
-//                while (activeThreads >= this->getSize()) {
-//                    std::cout << "kontynuuje czekanie " << activeThreads << std::endl;
-//                    condActive.wait(lock);
-//                }
             }
             activeThreads++;
         }
@@ -129,9 +108,6 @@ ContestResult TeamNewThreads::runContestImpl(ContestInput const &contestInput) {
             });
         }
     }
-    //for (size_t j = 0; j < contestInput.size(); j++) {
-    //    std::cout << contestInput[j] << ' ' << r[j] << '\n';
-    //}
 
     return r;
 }
@@ -148,43 +124,26 @@ TeamConstThreads::runContestImpl(ContestInput const &contestInput) {
     ContestInput::const_iterator inputIt = contestInput.begin();
     ContestResult::iterator resIt = r.begin();
     std::vector<std::thread> threads;
-    //std::cout << "startowanie wątków" << std::endl;
     for (size_t i = 0; i < this->getSize(); i++) {
         size_t s = forEach;
         if (additional > 0) {
             s++;
             additional--;
         }
-        //std::cout << "przed startem wątku" << std::endl;
         std::thread t = createThread(teamConstThreadsThread,
                                      inputIt,
                                      resIt,
                                      s,
                                      this->getSharedResults()
-//                                     &mut,
-//                                     &cv,
-//                                     &active_threads
                                      );
         threads.push_back(std::move(t));
-        //std::cout << "po starcie" << std::endl;
         inputIt = inputIt + s;
         resIt = resIt + s;
-        //std::cout << "po inkrementacji" << std::endl;
     }
-//    {
-//        std::unique_lock<std::mutex> lock(mut);
-//        std::cout << "main lock" << std::endl;
-//        cv.wait(lock, [&active_threads]{return active_threads == 0;});
-//    }
+
     for (auto &i : threads)
         i.join();
-//    for (auto a : contestInput)
-//        std::cout << a << ' ';
-//    std::cout << std::endl;
-//    for (auto a : r)
-//        std::cout << a << ' ';
-    //std::cout << std::endl;
-    //std::cout << "main end" << std::endl;
+
     return r;
 }
 
@@ -205,19 +164,15 @@ ContestResult TeamPool::runContest(ContestInput const &contestInput) {
 }
 
 void saveFromPipe(int desc, ContestResult &result) {
-    //std::cout << desc << " saving from pipe" << std::endl;
     uint64_t singleResult;
     int readOut = 1;
     while (readOut) {
         readOut = read(desc, &singleResult, sizeof(uint64_t));
-        //std::cout << readOut << std::endl;
         if (readOut == -1)
             assert(false);
         else if (readOut != 0) {
             result.push_back(singleResult);
-            //std::cout << singleResult << std::endl;
         }
-        //std::cout << desc << " " << readOut << std::endl;
     }
 }
 
@@ -252,7 +207,6 @@ ContestResult TeamNewProcesses::runContest(ContestInput const &contestInput) {
     }
 
     for (auto singleInput : contestInput) {
-        //std::cout << singleInput << std::endl;
         if (activeProcesses == getSize()) {
             auto desc = readDesc[savedResults];
             savedResults++;
@@ -272,49 +226,33 @@ ContestResult TeamNewProcesses::runContest(ContestInput const &contestInput) {
         switch (fork()) {
             case -1:
                 syserr("error in fork");
-                //assert(false);
             case 0:
-                /*if (close(0) == -1)
-                    syserr("err in closing standard input");
-                if (close(1) == -1)
-                    syserr("err in closing standard output");*/
                 if (dup2(pipe1_desc[PIPE_READ], 0) == -1) // input dziecka
-                    //assert(false);
                     syserr("error in dup2 input");
                 if (dup2(pipe2_desc[PIPE_WRITE], 1) == -1) // output dziecka
-                    //assert(false);
                     syserr("error in dup2 output");
 
                 // zamykanie niepotrzebnych deskryptorów
                 if (close(pipe1_desc[PIPE_READ]) == -1)
-                    //assert(false);
                     syserr("error in close(pipe1_desc[0])");
                 if (close(pipe1_desc[PIPE_WRITE]) == -1)
-                    //assert(false);
                     syserr("error in close(pipe1_desc[1])");
                 if (close(pipe2_desc[PIPE_READ]) == -1)
-                    //assert(false);
                     syserr("error in close(pipe2_desc[0])");
                 if (close(pipe2_desc[PIPE_WRITE]) == -1)
-                    //assert(false);
                     syserr("error in close(pipe2_desc[1])");
 
                 execlp("./new_process", "new_process", (char*)NULL);
-                //assert(false);
                 syserr("error in execlp");
                 break;
             default:
                 if (close(pipe1_desc[PIPE_READ]) == -1)
-                    //assert(false);
                     syserr("error in close(pipe1_desc[0])");
                 if (close(pipe2_desc[PIPE_WRITE]) == -1)
                     syserr("error in close(pipe2_desc[1])");
-                    //assert(false);
 
                 readDesc.push_back(pipe2_desc[PIPE_READ]);
-                //int write = pipe1_desc[1];
                 std::string asString = singleInput.toString();
-                //std::cout << singleInput << " = " << asString.size() << " -> " << calcCollatz(singleInput) << std::endl;
                 char is_shared = (bool)getSharedResults() ? 't' : 'f';
                 write(pipe1_desc[PIPE_WRITE], &is_shared, sizeof(char));
                 if (write(pipe1_desc[PIPE_WRITE], asString.c_str(), sizeof(char) * asString.size()) == -1)
@@ -367,6 +305,5 @@ ContestResult TeamAsync::runContest(ContestInput const &contestInput) {
     }
     for (auto &i : results)
         r.push_back(i.get());
-    return r;
     return r;
 }
